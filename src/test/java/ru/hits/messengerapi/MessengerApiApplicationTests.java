e92dc1a098a6fff15ab4f13a9c7c3b11913de07c
iseeyouwatching <leonidshibanovv@gmail.com>
2023-03-27 19:37:36 +0700

    Инициализировал проект

---
+++ src/test/java/ru/hits/messengerapi/MessengerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hits.messengerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
