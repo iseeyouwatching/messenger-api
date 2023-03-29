384d683d4318962dc28688c81b5fd4d1c5d0a86e
iseeyouwatching <leonidshibanovv@gmail.com>
2023-03-29 18:04:01 +0700

    Добавил метод по поиску UserEntity по логину в репозиторий пользователя

--- src/main/java/ru/hits/messengerapi/repository/UserRepository.java
+++ src/main/java/ru/hits/messengerapi/repository/UserRepository.java
@@ -2,10 +2,12 @@ package ru.hits.messengerapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.hits.messengerapi.entity.User;
+import ru.hits.messengerapi.entity.UserEntity;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    UserEntity findByLogin(String login);
 }
