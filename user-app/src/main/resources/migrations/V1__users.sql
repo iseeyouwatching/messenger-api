fced5e926923ce86e06406c483d548dcc1d07d90
iseeyouwatching <leonidshibanovv@gmail.com>
2023-04-11 13:46:26 +0700

    Добавил миграции

---
+++ user-app/src/main/resources/migrations/V1__users.sql
@@ -0,0 +1,22 @@
+create table public._user
+(
+    id                uuid not null
+        primary key,
+    avatar            uuid,
+    birth_date        date,
+    city              varchar(255),
+    email             varchar(255)
+        constraint email_constraint
+            unique,
+    full_name         varchar(255),
+    login             varchar(255)
+        constraint login_constraint
+            unique,
+    password          varchar(255),
+    phone_number      varchar(255),
+    registration_date timestamp
+);
+
+alter table public._user
+    owner to hits;
+
