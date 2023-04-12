e88e1bb3b4d66a1102ad0d284b231f01aeb62a62
iseeyouwatching <leonidshibanovv@gmail.com>
2023-04-12 13:14:09 +0700

    Добавил дополнительную валидацию полей при регистрации

--- user-app/src/main/java/ru/hits/messengerapi/user/dto/UserSignUpDto.java
+++ user-app/src/main/java/ru/hits/messengerapi/user/dto/UserSignUpDto.java
@@ -2,10 +2,7 @@ package ru.hits.messengerapi.user.dto;
 
 import lombok.*;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -23,12 +20,14 @@ public class UserSignUpDto {
      * Логин пользователя.
      */
     @NotBlank(message = "Логин не может быть пустым.")
+    @NotNull(message = "Логин является обязательным к заполнению.")
     private String login;
 
     /**
      * Пароль пользователя.
      */
     @NotBlank(message = "Пароль не может быть пустым.")
+    @NotNull(message = "Пароль является обязательным к заполнению.")
     @Size(min = 8, max = 32, message = "Длина пароля должна быть от 8 до 32 символов.")
     private String password;
 
@@ -36,12 +35,15 @@ public class UserSignUpDto {
      * Почта пользователя.
      */
     @NotBlank(message = "Почта не может быть пустой.")
+    @NotNull(message = "Почта является обязательной к заполнению.")
     @Email(message = "Некорректный формат почты.")
     private String email;
 
     /**
      * Полное имя пользователя.
      */
+    @NotBlank(message = "ФИО не может быть пустым.")
+    @NotNull(message = "ФИО является обязательным к заполнению.")
     private String fullName;
 
     /**
