bbc908f59b1ad959fae87c09f871854a915db8a1
iseeyouwatching <leonidshibanovv@gmail.com>
2023-05-14 22:28:44 +0700

    Добавил миграции в сервис файлового хранилища

---
+++ file-storage-app/src/main/resources/migrations/V1__create_file_metadata_table.sql
@@ -0,0 +1,14 @@
+create table public.file_metadata
+(
+    id            uuid not null
+        primary key,
+    bucket        varchar(255),
+    content_type  varchar(255),
+    creation_date date,
+    filename      varchar(255),
+    object_name   uuid
+);
+
+alter table public.file_metadata
+    owner to hits;
+
